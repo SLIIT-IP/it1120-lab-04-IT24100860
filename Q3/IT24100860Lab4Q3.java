bbf3d61497c3591c478d4d5efd82b2dd15649e07
IT24100860 <IT24100860@my.sliit.lk>
2024-08-16 23:33:22 +0530

    IT24100860Lab4Q3.java

--- Q3/IT24100860Lab4Q3.java
+++ Q3/IT24100860Lab4Q3.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+public class IT24100860Lab4Q3
+{
+	public static void main(String[]args)
+	{
+		Scanner input=new Scanner(System.in);
 
-public class IT24100860Lab4Q3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+		int num;
+		String result;
 
-        // Prompt the user to input a number
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+		System.out.println("Enter a number: ");
+		num=input.nextInt();
 
-        // Determine if the number is positive, negative, or zero using the ternary operator
-        String result = (number > 0) ? "The number is Positive"
-                      : (number < 0) ? "The number is Negative"
-                      : "The number is Zero";
-
-        // Display the result
-        System.out.println(result);
-
-        // Close the scanner
-        scanner.close();
-    }
+		result=(num>0)?"The number is:Positive":
+		       (num<0)?"The number is:Negative":
+		       "The number is:Zero";
+		System.out.println(result);
+	}
 }
